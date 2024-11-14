b8c43fed9d3e7569fddfdb9ad905a80448cd85ca
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-11-14 10:19:29 +0100

    Update Animalfactory.cs

--- Animalfactory.cs
+++ Animalfactory.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Dag_5
+class AnimalFactory
 {
-    internal class Animalfactory
+    // Metode med navneparameter
+    public static Animals CreateAnimal(enmAnimals animalType, string navn)
     {
+        return animalType switch
+        {
+            enmAnimals.Dog => new Dog(navn),
+            enmAnimals.Cat => new Cat(navn),
+            enmAnimals.Sheep => new Sheep(navn),
+            _ => throw new ArgumentException("Ugyldig dyretype")
+        };
+    }
+
+    // Overload metode uden navneparameter
+    public static Animals CreateAnimal(enmAnimals animalType)
+    {
+        string navn = animalType switch
+        {
+            enmAnimals.Dog => "Hund",
+            enmAnimals.Cat => "Kat",
+            enmAnimals.Sheep => "Får",
+            _ => "Ukendt"
+        };
+        return CreateAnimal(animalType, navn);
     }
 }
