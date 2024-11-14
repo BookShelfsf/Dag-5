de72fa2ea389329d3e4524d8921c05f391b4234a
BookShelfsf <166497948+BookShelfsf@users.noreply.github.com>
2024-11-14 10:17:37 +0100

    Add files via upload

---
+++ Dog, Cat, Sheep.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_5
+{
+    internal class Dog__Cat__Sheep
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Opret objekter via factory klasse
+        Animals dog = AnimalFactory.CreateAnimal(enmAnimals.Dog, "Hund");
+        Animals cat = AnimalFactory.CreateAnimal(enmAnimals.Cat, "Kat");
+        Animals sheep = AnimalFactory.CreateAnimal(enmAnimals.Sheep, "Får");
+
+        // Kald MakeSound() metoden
+        dog.MakeSound();   // Output: Bob siger: Vov!
+        cat.MakeSound();   // Output: Kitty siger: Meow!
+        sheep.MakeSound(); // Output: Dolly siger: Baaaah!
+    }
+}
